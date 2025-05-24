a70425d368d989e12ce61afc76b4b0a13b902673
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2025-05-24 16:51:01 -0300

    Sexto commit: Ajustes Docker

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
-COPY backend/ /app/
+COPY . /app/
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
--- api_monitoramento/settings.py
+++ api_monitoramento/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'api_monitoramento.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
